3fcaa15143ebe257ca3798c97d734cd53ed49d75
Ruchit <56220571+ruchit-t@users.noreply.github.com>
2023-10-08 18:12:47 -0400

    Update main.py
    Added New Design and Basic Structure

--- main.py
+++ main.py
@@ -9,7 +9,32 @@ import time
 from datetime import datetime, timedelta
 import altair as alt
 
+def get_category():
+    category_dict = dict()
+    category_json_list = rq.get("https://opentdb.com/api_category.php").json()
+    for item in category_json_list['trivia_categories']:
+        category_dict[item['id']] = item['name']
+    return category_dict
+
+
+
 st.markdown("<style>description {color: Green;}</style>",unsafe_allow_html = True)
-st.title("Welcome to the Quiz Master!")
-st.markdown("---")
-st.sidebar.title("Choose one of the following category of Quiz:")
+st.title(":orange[Welcome to the] :violet[QuizMaster!]")
+st.subheader("_Engage, Entertain, and Educate with QuizMaster - Where Knowledge Meets Fun!_", divider= 'rainbow')
+# st.markdown("---")
+st.sidebar.title("Tune the Options to Play the Game")
+st.sidebar.markdown("---")
+categories_option = get_category()
+category = st.sidebar.selectbox("Category: ", list(categories_option.values()), index= None, placeholder= "Select one: ")
+if category is None:
+    st.warning('Please select one category to start the game', icon="⚠️")
+else:
+    levels = st.sidebar.selectbox("Difficulty Level: ", ['Easy', 'Medium', 'Hard'])
+    QuestionCount = st.sidebar.slider("Number of Question Per Game:", 1, 50, 1)
+    TypeOption = st.sidebar.selectbox("Type of Questions", ['True/False', "MCQ"])
+    if TypeOption == 'True/False':
+        TypeOption = 'boolean'
+    else:
+        TypeOption = 'multiple'
+    
+

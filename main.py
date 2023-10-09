b2ca6af53f6766a9f1eac7895e3423506e15d54d
Ruchit <56220571+ruchit-t@users.noreply.github.com>
2023-10-09 17:50:53 -0400

    Update main.py
    Changed dictionary structure and logic

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ def get_category():
     category_dict = dict()
     category_json_list = rq.get("https://opentdb.com/api_category.php").json()
     for item in category_json_list['trivia_categories']:
-        category_dict[item['id']] = item['name']
+        category_dict[item['name']] = item['id']
     return category_dict
 
+def get_question(amount,category,difficulty):
+    questions = rq.get("https://opentdb.com/api.php?amount="+str(amount)+"&category="+str(category)+"&difficulty="+str(difficulty)).json()
+    return questions
 
 
 st.markdown("<style>description {color: Green;}</style>",unsafe_allow_html = True)
@@ -25,16 +28,17 @@ st.subheader("_Engage, Entertain, and Educate with QuizMaster - Where Knowledge 
 st.sidebar.title("Tune the Options to Play the Game")
 st.sidebar.markdown("---")
 categories_option = get_category()
-category = st.sidebar.selectbox("Category: ", list(categories_option.values()), index= None, placeholder= "Select one: ")
+category = st.sidebar.selectbox("Category: ", list(categories_option.keys()), index= None, placeholder= "Select one: ")
 if category is None:
     st.warning('Please select one category to start the game', icon="⚠️")
 else:
     levels = st.sidebar.selectbox("Difficulty Level: ", ['Easy', 'Medium', 'Hard'])
-    QuestionCount = st.sidebar.slider("Number of Question Per Game:", 1, 50, 1)
-    TypeOption = st.sidebar.selectbox("Type of Questions", ['True/False', "MCQ"])
-    if TypeOption == 'True/False':
-        TypeOption = 'boolean'
-    else:
-        TypeOption = 'multiple'
-    
+    QuestionCount = st.sidebar.slider("Number of Question Per Game:", 1, 10, 1)
+    # TypeOption = st.sidebar.selectbox("Type of Questions", ['Any','True/False', "MCQ"])
+    # if TypeOption == 'True/False':
+    #     TypeOption = 'boolean'
+    # else:
+    #     TypeOption = 'multiple'
+    QuestionList = get_question(QuestionCount,categories_option[category], levels.lower())
+    st.write(QuestionList)
 
